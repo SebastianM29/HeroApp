01e93127c7a94474d781b3bffe678fa2f94b9407
Sebastian <zzevazzz_1@hotmail.com>
2023-11-09 10:45:07 -0300

    heroPushActualizado7

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
-  build: {
-    outDir: './docs'
-  }
+  base: "https://SebastianM29.github.io/heroApp",
+
 
 })
